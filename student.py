f28408d45bff2e1b26d4458e3547d36f8d3d5c2c
roc-11 <>
2024-01-24 23:13:39 +0000

    Use mock method to make request from a fictional external API so successful and failed responses can be tested

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
@@ -26,3 +27,15 @@ class Student:
 
     def apply_extension(self, days):
         self.end_date = self.end_date + timedelta(days=days)
+
+    def course_schedule(self):
+        fname = self._first_name
+        lname = self._last_name
+
+        response = requests.get(
+            f"http://company.com/course-schedule/{lname}/{fname}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
--- test_student.py
+++ test_student.py
@@ -1,6 +1,7 @@
 import unittest
 from student import Student
 from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
@@ -43,6 +44,26 @@ class TestStudent(unittest.TestCase):
 
         self.assertEqual(
             self.student.end_date, old_end_date + timedelta(days=5))
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failed(self):
+        """
+        In the path "student" comes from the name of the file student.py
+        If you have named the file something else - it will need to match
+        eg, students.py would need "students.requests.get"
+        """
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong")
 
 
 if __name__ == '__main__':
